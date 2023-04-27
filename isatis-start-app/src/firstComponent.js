42a22b825cdcf7d73f9c60907e8bbbb372b7c92a
fabianfransen <fabianfransen96@hotmail.com>
2023-04-28 00:00:14 +0200

    component class

---
+++ isatis-start-app/src/firstComponent.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+class TestClass extends Component {
+    render() {
+        return(<div>
+            <h1> Deze text komt uit een component </h1>
+        </div>)
+    }
+}
+
+export default TestClass
