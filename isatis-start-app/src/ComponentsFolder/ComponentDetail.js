6d12826aa137eb6c235f067f6467a451282964c4
fabianfransen <fabianfransen96@hotmail.com>
2023-04-28 19:21:27 +0200

    added button that toggles content visibility
    the button toggles only the state of the content while keeping it rendered

--- isatis-start-app/src/ComponentsFolder/ComponentDetail.js
+++ isatis-start-app/src/ComponentsFolder/ComponentDetail.js
@@ -6,6 +6,11 @@ class ComponentDetail extends Component {
     constructor(props){
         super(props)
         this.ensembleIDClickEvent = this.ensembleIDClickEvent.bind(this)
+        this.toggleContent = this.toggleContent.bind(this)
+        this.state = {
+            showContent: true
+        }
+
     }
     ensembleIDClickEvent(event){
         event.preventDefault()
@@ -15,14 +20,26 @@ class ComponentDetail extends Component {
             dataCallback("callback")
         }
     }
+
+    toggleContent (event) {
+        // prevent the default
+        event.preventDefault()
+        this.setState({
+            showContent: !this.state.showContent
+        })
+    }
     render () {
         const {gene} = this.props
+        const {showContent} = this.state
         return (
             <div>
                 <h1 onClick={this.ensembleIDClickEvent}> {gene.ensemblID} </h1>
-                <p>{gene.geneName}</p>
-                <p>{gene.speciesName}</p>
-                <p>{gene.source}</p>
+                // d-block bootstrap
+                /<p className={` ${showContent === true ? 'd-block' : 'd-none' }`}>{gene.geneName}</p>                                          
+                {showContent === true ? <p>{gene.geneName}</p> : ""}
+                {showContent === true ? <p>{gene.speciesName}</p> : ""}
+                {showContent === true ? <p>{gene.source}</p> : ""}
+                <button onClick={this.toggleContent}> Toggle content display </button>
             </div>
         )
     }
