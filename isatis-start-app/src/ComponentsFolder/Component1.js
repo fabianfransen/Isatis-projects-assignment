2bcde1acc414668b622d9f2b4e8bb0d2e81c2556
fabianfransen <fabianfransen96@hotmail.com>
2023-04-28 17:57:50 +0200

    added event and handler
    eenvoudige onClick event met een callback functie binnen het event.

--- isatis-start-app/src/ComponentsFolder/Component1.js
+++ isatis-start-app/src/ComponentsFolder/Component1.js
@@ -6,16 +6,27 @@ import TestData from '../data/testData.json'
 import ComponentDetail from './ComponentDetail';
 
 class Component1 extends Component {
+    constructor(props){
+        super(props)
+        this.dataCallbackHandler = this.dataCallbackHandler.bind(this)
+    }
+    dataCallbackHandler(text){
+        // alert(text)
+        // console.log(this)
+    }
     render () {
         return (
             <div >
                 <h1> Hello there</h1>
                 {TestData.map((savedGene, index)=>{
-                    return <ComponentDetail gene={savedGene} key={`gene-list-key ${index}`}/>
+                    return <ComponentDetail 
+                    gene={savedGene} 
+                    key={`gene-list-key ${index}`} 
+                    dataCallback={this.dataCallbackHandler} />
                 })}
             </div>
         )
     }
- }
+ } 
 
 export default Component1;
--- isatis-start-app/src/ComponentsFolder/ComponentDetail.js
+++ isatis-start-app/src/ComponentsFolder/ComponentDetail.js
@@ -3,11 +3,23 @@ import React, { Component } from 'react';
 // import './App.css';
 
 class ComponentDetail extends Component {
+    constructor(props){
+        super(props)
+        this.ensembleIDClickEvent = this.ensembleIDClickEvent.bind(this)
+    }
+    ensembleIDClickEvent(event){
+        event.preventDefault()
+        console.log(this.ensembleIDClickEvent)
+        const {dataCallback} = this.props
+        if (dataCallback !== undefined) {
+            dataCallback("callback")
+        }
+    }
     render () {
         const {gene} = this.props
         return (
             <div>
-                <h1>{gene.ensemblID} </h1>
+                <h1 onClick={this.ensembleIDClickEvent}> {gene.ensemblID} </h1>
                 <p>{gene.geneName}</p>
                 <p>{gene.speciesName}</p>
                 <p>{gene.source}</p>
